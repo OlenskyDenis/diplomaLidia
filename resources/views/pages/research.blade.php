ce080af54ee3bff74df4b853f44ae20a3decf6ef
obozna <94908691+obozna@users.noreply.github.com>
2021-12-12 01:09:12 +0200

    change Research and Contact pages

--- resources/views/pages/research.blade.php
+++ resources/views/pages/research.blade.php
@@ -15,21 +15,41 @@
                 </div>
                 <div class="col-lg-6 pt-4 pt-lg-0">
                     <h3>Аналіз ґрунтових вод родовищ Житомирського регіону</h3>
-                    <p class="fst-italic">
+                    <p class="fst-italic mt-4">
                         Якість води – це поєднання хімічного і біологічного складу та фізичних властивостей води, що
                         зумовлює її придатність для конкретних видів водокористування. Встановлені значення показників
                         якості води (фізичні, хімічні та біологічні) називаються нормами якості води і відповідають певним
                         стандартам.
                     </p>
+                </div>
+                <div class="col-lg-12 mt-3 pt-lg-0">
                     <ul>
-                        <li><i class="bi bi-check2-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                        <li><i class="bi bi-check2-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.
+                        <li><i class="bi bi-check2-circle"></i> Лактозопозитивні кишкові палички (ЛКП) – мікробне забруднення
+                            водойм, при яких небезпечно вживати
+                            воду або навіть перебувати у ній.
                         </li>
-                        <li><i class="bi bi-check2-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                            aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat
-                            nulla pariatur.</li>
-                    </ul>
+                        <li><i class="bi bi-check2-circle"></i> Біохімічне споживання кисню (БСК5) – біохімічне споживання
+                            кисню в річковій воді вимірюється у
+                            мг О2/л, що відображає споживання кисню для окислення легкоокислюваних органічних речовин
+                            впродовж 5
+                            діб (БСК5).
+                        </li>
+                        <li><i class="bi bi-check2-circle"></i> Хімічне споживання кисню (ХСК) - кількість кисню у мг/дм3
+                            (або іншого окисника у розрахунку на
+                            кисень), яка необхідна для повного окиснення органічних речовин, що містяться в пробі води; такі
+                            елементи як, С, H, S, P та інші (крім азоту), які присутні в органічній речовині, окиснюються до
+                            CO2, H2O, P2O5, SO3.
+                        </li>
+                        <li><i class="bi bi-check2-circle"></i> Розчинний кисень (РК) - вміст розчиненого кисню в природній
+                            воді коливається в межах від 0 до 14
+                            мгО2/дм3.
+                        </li>
+                        <li><i class="bi bi-check2-circle"></i> Вміст кислотності (pH) – вміст у воді речовин, здатних
+                            вступати в реакцію з гідроксид-іонами
+                            (ОН-).
+                        </li>
 
+                    </ul>
                 </div>
             </div>
 
